9396015908db7b90c12f838bfdb0a90ae4248482
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-09 15:46:08 +0100

    control system to print alaments on homepage

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     //
     public function index(){
-        return view('home');
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,15 @@
 
     <main class="bg-light">
         <h1>Home</h1>
+        <div class="container">
+            <div class="row">
+                @foreach ( $movies as $movie)
+                    <div class="col-4 p-4">
+                        <h4>{{$movie->title}}</h4>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </main>
 </body>
 
