dfdec521e698669cb35e00bc3f0eaa695de1e9e4
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-09 16:12:22 +0100

    separated main content from layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
+@extends('layouts.app')
+@section('main-content')
     <main class="bg-light">
         <h1>Home</h1>
         <div class="container">
@@ -29,6 +12,4 @@
             </div>
         </div>
     </main>
-</body>
-
-</html>
+@endsection
